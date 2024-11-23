1d62f156fac163442cb1112f0d6d8274f6420886
Your Name <tunmiseelisha98@gmail.com>
2024-11-23 07:29:34 +0100

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-
-let name: string;
-let age: number;
-let isStudent: boolean;
-let hobbies:string[];
-let role:[number, string]
-
-let printName: (name: string) => never;
+import InputField from './components/InputField';
 
 
 const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("")
+
+  console.log(todo)
+
   return (
     <div className="App">
       <div className='header'>
         <span className='heading'>Taskify</span>
       </div>
+      <InputField todo={todo} setTodo={setTodo}/>
     </div>
   );
 }
---
+++ src/components/InputField.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './style.css'
+
+interface Props{
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField = ({todo, setTodo} : Props) => {
+  return (
+    <form className='input'>
+        <input type="input" 
+        value={todo}
+        onChange={
+            (e)=>setTodo(e.target.value)
+        }
+        placeholder='Enter a task'
+        className='input__box'/>
+        <button className='input__submit' type='submit'>Go</button>
+    </form>
+  )
+}
+
+export default InputField
